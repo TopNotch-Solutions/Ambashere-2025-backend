4458ee2a64ea027a5b517e6a656e4a68381d15e8
Paulus <pwilhem@mtc.com.na>
2026-06-04 08:38:19 +0200

    .env

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
 const Sequelize = require('sequelize');
+require("dotenv").config();
 
 const sequelize = new Sequelize({
-  host: "172.19.13.140",
-  username: "dbAdmin",
-  password: "Ambasphere",
-  database: "airtimemanagement",
+  host: process.env.HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB,
   dialect: "mysql",
   // Sequelize-only option (do not pass via dialectOptions — MySQL2 rejects it)
   attributeBehavior: "unsafe-legacy",
